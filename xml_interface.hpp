ca42440cb341b9f2e1e15e354828c8ee191035dd
elkesh <84234021+elkesh@users.noreply.github.com>
2021-09-15 12:03:18 +0300

    Delete xml_interface.hpp

--- xml_interface.hpp
+++
@@ -1,6 +0,0 @@
-using namespace std;
-#include <opencv2/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-void parser_func(int** rectangles,double scale,cv::Mat &image);
-
-const char* caster(string a);
