c42b2d1db107d2900a8678a6ceab48bf2c67dee4
elkesh <84234021+elkesh@users.noreply.github.com>
2021-09-15 12:03:35 +0300

    Delete map_generator.hpp

--- map_generator.hpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-using namespace cv;
-using namespace std;
-
-
-void generate_map(string model, const char* pose,Mat &image,double scale);
-char** string_parser(const char* head, int size);
-bool compare_string_with_ptrtochar(string a,const char* b);
