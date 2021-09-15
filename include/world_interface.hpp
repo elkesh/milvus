4671fe65f2f70ce3dc626200e29fc42a1f594696
elkesh <84234021+elkesh@users.noreply.github.com>
2021-09-15 12:01:45 +0300

    Add files via upload

---
+++ include/world_interface.hpp
@@ -0,0 +1,4 @@
+#include <opencv2/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+void generator(double rows,double colms,double scale,Mat &image);
---
+++ include/xml_interface.hpp
@@ -0,0 +1,6 @@
+using namespace std;
+#include <opencv2/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+void parser_func(int** rectangles,double scale,cv::Mat &image);
+
+const char* caster(string a);
