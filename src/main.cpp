acb4e87e20860167372dda67bc2c6c7f93ae28fa
elkesh <84234021+elkesh@users.noreply.github.com>
2021-09-15 11:59:46 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include "rectangles_interface.hpp"
+#include "xml_interface.hpp"
+#include "world_interface.hpp"
+#include "tinyxml2.h"
+using namespace tinyxml2;
+using namespace std;
+using namespace cv;
+
+vector<double> gauss(vector< vector<double> > A) {
+    int n = A.size();
+
+    for (int i=0; i<n; i++) {
+        // Search for maximum in this column
+        double maxEl = abs(A[i][i]);
+        int maxRow = i;
+        for (int k=i+1; k<n; k++) {
+            if (abs(A[k][i]) > maxEl) {
+                maxEl = abs(A[k][i]);
+                maxRow = k;
+            }
+        }
+
+        // Swap maximum row with current row (column by column)
+        for (int k=i; k<n+1;k++) {
+            double tmp = A[maxRow][k];
+            A[maxRow][k] = A[i][k];
+            A[i][k] = tmp;
+        }
+
+        // Make all rows below this one 0 in current column
+        for (int k=i+1; k<n; k++) {
+            double c = -A[k][i]/A[i][i];
+            for (int j=i; j<n+1; j++) {
+                if (i==j) {
+                    A[k][j] = 0;
+                } else {
+                    A[k][j] += c * A[i][j];
+                }
+            }
+        }
+    }
+    // Solve equation Ax=b for an upper triangular matrix A
+    vector<double> x(n);
+    for (int i=n-1; i>=0; i--) {
+        x[i] = A[i][n]/A[i][i];
+        for (int k=i-1;k>=0; k--) {
+            A[k][n] -= A[k][i] * x[i];
+        }
+    }
+    return x;
+}
+
+int main(){
+/*sil!!!!!!!!!!!!!!!!!!!!!!
+    vector<double> line(3);
+    vector< vector<double> > A(2,line);
+    A[0][0]=1;A[0][1]=2;A[0][2]=0;
+    A[1][0]=2;A[1][1]=-2;A[1][2]=6;
+    vector<double>x(2);x=gauss(A);
+    cout<<x[0]<<"  "<<x[1]<<endl;*/
+
+    
+    cv::Mat image;
+    cv::Mat binary_image;
+    image = cv::imread("deneme.jpg");
+
+    flip(image,image,0);
+    int rows = image.rows;
+    int cols = image.cols;
+
+    threshold(image,binary_image,100,255,CV_THRESH_BINARY);
+
+    double scale;
+    cout<<"Enter the scale between the image and simulation world scale(meter per pixel)"<<endl;
+    cin>>scale;
+
+    int **ptr_to_rect=generate_borders(binary_image);
+    
+    parser_func(ptr_to_rect,scale,image);
+
+    generator((double)rows, (double)cols,scale,image);
+
+    flip(image,image,0);
+    cout<<"no segfault"<<endl;
+    for(int i=0;i<rows;i++){
+
+        image.at<Vec3b>(i,cols/2)=Vec3b(0,0,255);
+
+    }for(int i=0;i<cols;i++){
+
+        image.at<Vec3b>(rows/2,i)=Vec3b(0,0,255);
+    }
+    imshow("uras",image);
+    waitKey(0);
+    imwrite("result.jpg",image);
+
+    
+    return 0;
+}
---
+++ src/map_png_generator.cpp
@@ -0,0 +1,230 @@
+#include "map_generator.hpp"
+#include "tinyxml2.h"
+#include <iostream>
+#include "xml_interface.hpp"
+#include "model_interface.hpp"
+#include <fstream>
+using namespace std;
+using namespace tinyxml2;
+
+
+XMLElement* xml_iterator(char* target, XMLElement* root){
+
+	XMLElement* current;
+	
+	string target_full="";
+	int i=0;
+	while(1){
+
+		if(target[i]=='\0')
+			break;
+		target_full=target_full+target[i];
+		i++;
+
+	}
+
+
+	if(root->Name()==target_full){
+		return root;	
+	}else{
+		if(root->FirstChildElement()!=0){
+
+			current = xml_iterator(target,root->FirstChildElement());
+			if(current!=0)
+				return current;
+		}
+		if(root->NextSiblingElement()!=0){			
+										
+			current = xml_iterator(target,root->NextSiblingElement());
+			if(current!=0)
+				return current;			
+		}
+		return 0;
+	}
+}
+bool compare_string_with_ptrtochar(string a,const char* b){
+
+	int index=0;
+	while(a[index]!='\0' && b[index]!='\0'){
+		if(a[index]!=b[index])
+			return false;
+		index++;
+	}
+	if(a[index]!=b[index])
+		return false;
+
+	return true;
+}
+
+
+char** string_parser(const char* head,int size){
+
+	char** return_string=nullptr;
+	return_string=new char*[size];
+	return_string[0]=new char[50];
+
+	int index=0;
+	int inner_index=0;
+	int head_index=0;
+
+	while(index<size){
+
+		if(head_index==0)
+			while(head[head_index]==' ')
+				head_index++;
+		
+		if(head[head_index]=='\0'){
+			return_string[index][inner_index]='\0';
+			break;
+		}
+		if(head[head_index]==' '){
+
+			return_string[index][inner_index]='\0';
+			index++;
+			if(index<size)
+				return_string[index]=new char[50];
+			inner_index=0;
+
+		}else{
+			return_string[index][inner_index]=head[head_index];
+			inner_index++;
+		}
+
+		head_index++;
+	}
+
+	return return_string;
+
+}
+
+
+void generate_map(string model, const char* pose,Mat &image,double scale){
+
+	XMLDocument xml_doc;
+	
+	XMLError eResult = xml_doc.LoadFile(caster("/usr/share/gazebo-9/models/"+model+"/model.sdf"));
+
+	XMLElement* root=xml_doc.FirstChildElement("sdf");
+
+	XMLElement* model_name = root->FirstChildElement("model");
+
+	XMLElement* link = model_name->FirstChildElement("link");
+
+	for(; link != 0; link=link->NextSiblingElement("link")){
+
+		XMLElement* iterator=xml_iterator("collision",link);
+		XMLElement* geo=xml_iterator("geometry",iterator);
+
+		char** pose_string = string_parser(pose,6);
+	
+		if(compare_string_with_ptrtochar("box",geo->FirstChildElement()->Name())) {//box
+			
+			XMLElement* size=xml_iterator("size",geo);
+			const char* size_value=size->GetText();
+
+			char** size_string=string_parser(size_value,3);
+
+			Box new_box=Box(pose_string[0],pose_string[1],pose_string[2],
+			pose_string[3],pose_string[4],pose_string[5],size_string[0],size_string[1]
+			,size_string[2]);
+
+			new_box.put_map(image,scale,false);
+
+		}else if(compare_string_with_ptrtochar("sphere",geo->FirstChildElement()->Name())){//sphere
+
+			XMLElement* radius=xml_iterator("radius",geo);
+			const char* radius_value=radius->GetText();
+
+			char** radius_string=string_parser(radius_value,1);
+
+			Sphere new_sphere=Sphere(pose_string[0],pose_string[1],pose_string[2],
+			pose_string[3],pose_string[4],pose_string[5],radius_string[0]);
+
+			new_sphere.put_map(image,scale,0);
+
+		}else if(compare_string_with_ptrtochar("cylinder",geo->FirstChildElement()->Name())){//cylinder
+
+
+			XMLElement* radius=xml_iterator("radius",geo);
+			XMLElement* height=xml_iterator("length",geo);
+			const char* radius_value=radius->GetText();
+			const char* height_value=height->GetText();
+
+			char** radius_string=string_parser(radius_value,1);
+			char** height_string=string_parser(height_value,1);
+
+			Cylinder new_cylinder=Cylinder(pose_string[0],pose_string[1],pose_string[2],
+			pose_string[3],pose_string[4],pose_string[5],radius_string[0]
+			,height_string[0]);
+
+			new_cylinder.put_map(image,1);
+		}else if(compare_string_with_ptrtochar("mesh",geo->FirstChildElement()->Name())){
+			
+			XMLElement* uri=xml_iterator("uri",geo->FirstChildElement());
+			
+			string filepath="/usr/share/gazebo-9/models/";			
+			
+			const char* remain=uri->GetText();
+
+			int index=0;
+			bool first_part=true;
+			while(remain[index]!='\0'){
+
+				if(!first_part)
+					filepath=filepath+remain[index];
+				if(index>0)
+					if (remain[index]=='/' && remain[index-1]=='/')
+						first_part=false;
+
+				index++;
+
+			}
+/*
+			char* whole_line=new char[200];
+			ifstream* infile;
+			infile.open(caster(filepath));
+			Point three_points[3];
+			bool start_flag=true;
+			int count_of_vertexes=0;*/
+
+			Mesh mesh_model=Mesh(pose_string[0],pose_string[1],pose_string[2],
+			pose_string[3],pose_string[4],pose_string[5]);
+
+			mesh_model.put_map(image,scale,filepath,0.21);
+/*
+			while(!infile.eof()){
+
+
+				infile.getline(whole_line,100);
+				char** line_disected=string_parser(whole_line,10);
+
+				if(compare_string_with_ptrtochar("vertex",line_disected[0])){
+
+					double x=(stod(line_disected[1])+stod(pose_string[0]))/scale;
+					double y=(stod(line_disected[2])+stod(pose_string[1]))/scale;
+
+					x=x+image.rows/2.0;
+					y=y+image.cols/2.0;
+
+					three_points[count_of_vertexes].x=x;
+					three_points[count_of_vertexes].y=y;
+					count_of_vertexes++;
+
+					if(count_of_vertexes==3){
+						
+						for(int q=0;q<3;q++){
+							line(image,three_points[q],three_points[(q+1)%3],(255,255,255),1);
+						}
+						count_of_vertexes=0;						
+					}
+
+				}
+
+			}*/
+
+		}
+		
+
+	}	
+
+}
